f21c3d41db8f0c3b1d30d254503203568c5a4a15
Александр Благий <sashkablag@icloud.com>
2025-12-24 01:09:04 +0300

    add bot token

--- wardrobe_app/config.py
+++ wardrobe_app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str
+    BOT_TOKEN: str = "8582666055:AAEH0QVbIAsmAfHV4EUN92333ojX_CPB0ck"
     DATABASE_URL: str = "sqlite+aiosqlite:///wardrobe_bot.db"
     WEATHERAPI_KEY: str = "f5ae1146d4c8afc02f9344e3a1f84edf"
 
