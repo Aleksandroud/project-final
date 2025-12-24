01d70453f649bc0930a3e9272bd979544a8c16a1
Александр Благий <sashkablag@icloud.com>
2025-12-24 15:03:12 +0300

    новый токен

--- wardrobe_app/config.py
+++ wardrobe_app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     BOT_TOKEN: str = "8582666055:AAEH0QVbIAsmAfHV4EUN92333ojX_CPB0ck"
     DATABASE_URL: str = "sqlite+aiosqlite:///wardrobe_bot.db"
     WEATHERAPI_KEY: str = "f5ae1146d4c8afc02f9344e3a1f84edf"
-    HUGGINGFACE_API_KEY: str="***"
+    HUGGINGFACE_API_KEY: str = "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"
 
     class Config:
         env_file = "../.env"
